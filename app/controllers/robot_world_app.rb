e3e5c64b79cd5990b0bbab721ef2e6cdb2b9bd96
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-24 18:06:29 -0700

    adds full CRUD functionality

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,7 +4,44 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robots])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+  set :method_override, true
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.delete(id.to_i)
+    redirect '/robots'
+  end
+
+  not_found do
+    erb :error
+  end
+
+end
