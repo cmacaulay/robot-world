56dfa3f9754c29951a550f92ce361821646ddfea
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-25 18:57:00 -0700

    adds styling to robot world

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,6 +3,7 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+
   get '/robots' do
     @robots = Robot.all
     erb :index
